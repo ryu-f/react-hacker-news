2e3932744b01221e0b59c70de2aefaadfab09736
ryu-f <r.furuta0926@gmail.com>
2019-06-19 01:12:25 +0900

    feat: 🎸 WIP changefeed apic

--- src/features/feed/components/NavigationView.tsx
+++ src/features/feed/components/NavigationView.tsx
@@ -2,14 +2,16 @@ import * as React from 'react'
 import Navigation from '@/componets/organisms/Navigation'
 import { navItem } from '@/data/navItem'
 import { useActions } from 'typeless'
-import { RouterActions } from 'typeless-router'
+import { FeedActions } from '@/features/feed/interface'
 
 const NavigationView: React.FC = () => {
-  const { push } = useActions(RouterActions)
+  const { changeFeed } = useActions(FeedActions)
 
   const onClick = (e: React.MouseEvent<HTMLElement>, i: number) => {
     e.preventDefault()
-    push(navItem[i].path)
+    const path = navItem[i].path
+    const type = navItem[i].type
+    changeFeed(path, type)
   }
 
   return <Navigation navItem={navItem} onClick={onClick} />
--- src/features/feed/interface.ts
+++ src/features/feed/interface.ts
@@ -5,6 +5,7 @@ import { FeedItem } from '@/types'
 export const [handle, FeedActions, getFeedState] = createModule(FeedSymbol)
   .withActions({
     $mounted: null,
+    changeFeed: (path: string, type: string) => ({ payload: { path, type } }),
     pushFeedItems: (feedItems: any) => ({ payload: { feedItems } }),
     changeType: (type: string) => ({ payload: { type } }),
     changePage: (page: number) => ({ payload: { page } })
