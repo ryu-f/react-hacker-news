8e821d562865c7a65d70c9935e0b486e63045497
ryu-f <r.furuta0926@gmail.com>
2020-01-06 13:00:22 +0900

    fix(atoms): atoms add link component

--- src/componets/atoms/Button/Button.tsx
+++ src/componets/atoms/Button/Button.tsx
@@ -1,46 +1,69 @@
 import * as React from 'react'
 
 import { COLOR, FONT_SIZE } from '@/styles/Constants'
+import { Link, RouteComponentProps } from 'react-router-dom'
 
 import styled from 'styled-components'
 
+// types
+////////////////////////////////////////////////////////////
 type Props = {
   size: 'BASE' | 'SMALL' | 'LARGE'
   textColor: 'BLACK' | 'WHITE'
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+interface ButtonProps extends Props, React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 interface AnchorProps extends Props, React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string
-  onClick?:
-    | (((event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void) &
-        ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void))
-    | undefined
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void
 }
 
-export const Button: React.FC<Props | AnchorProps> = props => {
-  const { size, textColor, children, onClick } = props
+interface LinkProps extends Props, RouteComponentProps {}
+
+type ComponentProps = ButtonProps | AnchorProps | LinkProps
+
+// jsx
+////////////////////////////////////////////////////////////
+export const Button: React.FC<ComponentProps> = props => {
+  const { size, textColor, children } = props
 
   if ('href' in props) {
-    const { href, onClick } = props
+    const { href } = props
 
     return (
-      <AnchorView size={size} textColor={textColor} href={href} onClick={onClick} {...props}>
+      <AnchorView size={size} textColor={textColor} href={href} {...props}>
         {children}
       </AnchorView>
     )
   }
 
+  if ('to' in props) {
+    const { to } = props
+
+    return (
+      <LinkView size={size} textColor={textColor} to={to} {...props}>
+        {children}
+      </LinkView>
+    )
+  }
+
   return (
-    <ButtonView size={size} textColor={textColor} onClick={onClick} {...props}>
+    <ButtonView size={size} textColor={textColor} {...props}>
       {children}
     </ButtonView>
   )
 }
 
+// styled
+////////////////////////////////////////////////////////////
 const ButtonView = styled.button<Props>`
   font-size: ${({ size }) => FONT_SIZE[size]}px;
   color: ${({ textColor }) => COLOR[textColor]};
 `
 
 const AnchorView = ButtonView.withComponent('a')
+
+const LinkView = ButtonView.withComponent(Link)
--- src/componets/atoms/Text/Text.tsx
+++ src/componets/atoms/Text/Text.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 
 import { COLOR, FONT_SIZE } from '@/styles/Constants'
+import { Link, RouteComponentProps } from 'react-router-dom'
 
 import styled from 'styled-components'
 
+// types
+////////////////////////////////////////////////////////////
 type Props = {
   size: 'BASE' | 'SMALL' | 'LARGE'
   textColor: 'BLACK' | 'WHITE' | 'GLAY'
@@ -14,32 +17,52 @@ interface AnchorProps extends Props, React.AnchorHTMLAttributes<HTMLAnchorElemen
 }
 
 interface HeadingProps extends Props, React.ParamHTMLAttributes<HTMLHeadingElement> {
-  level: 1 | 2 | 3 | 4 | 5 | 6
+  level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 }
 
-export const Text: React.FC<Props | AnchorProps | HeadingProps> = props => {
+interface LinkProps extends Props, RouteComponentProps {}
+
+type ComponentProps = Props | AnchorProps | HeadingProps | LinkProps
+
+// jsx
+////////////////////////////////////////////////////////////
+export const Text: React.FC<ComponentProps> = props => {
   const { size, textColor, children } = props
 
   if ('href' in props) {
     const { href } = props
 
     return (
-      <AnchorView size={size} textColor={textColor} href={href}>
+      <AnchorView size={size} textColor={textColor} href={href} {...props}>
         {children}
       </AnchorView>
     )
   }
 
+  if ('to' in props) {
+    const { to } = props
+
+    return (
+      <LinkView size={size} textColor={textColor} to={to} {...props}>
+        {children}
+      </LinkView>
+    )
+  }
+
   return (
-    <TextView size={size} textColor={textColor}>
+    <TextView size={size} textColor={textColor} {...props}>
       {children}
     </TextView>
   )
 }
 
+// styled
+////////////////////////////////////////////////////////////
 const TextView = styled.p<Props>`
   font-size: ${({ size }) => FONT_SIZE[size]}px;
   color: ${({ textColor }) => COLOR[textColor]};
 `
 
 const AnchorView = TextView.withComponent('a')
+
+const LinkView = TextView.withComponent(Link)
