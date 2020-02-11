1d7d826f9d9473547fa53e87783098d5c0723a3e
ryu-f <r.furuta0926@gmail.com>
2020-02-11 17:18:52 +0900

    feat(package): add react-spring@next

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
         <title>React Hacker News</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
-          href="https://fonts.googleapis.com/css?family=Oswald+Roboto&display=swap"
+          href="https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap"
           rel="stylesheet"
         />
       </Helmet>
