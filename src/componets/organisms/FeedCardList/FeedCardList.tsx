77f0f17d9f3e49e28cd07a1365fd77a88743abc4
ryu-f <r.furuta0926@gmail.com>
2020-03-03 19:25:58 +0900

    refactor(organism): add react-transition-group

--- src/componets/organisms/FeedCardList/FeedCardList.tsx
+++ src/componets/organisms/FeedCardList/FeedCardList.tsx
@@ -1,3 +1,4 @@
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { shallowEqual, useSelector } from 'react-redux'
 
 import FeedCard from '@/componets/organisms/FeedCard'
@@ -11,20 +12,28 @@ type Props = {
   feedItem: FeedItem[]
 }
 
-const View: React.FC<Props> = ({ feedItem }) => (
-  <LayoutGrid>
-    {feedItem.map((card, i) => (
-      <li key={`card-${i}`}>
-        <FeedCard
-          title={card.title}
-          user={card.user}
-          time={card.time_ago}
-          comments={card.comments_count}
-        />
-      </li>
-    ))}
-  </LayoutGrid>
-)
+const View: React.FC<Props> = ({ feedItem }) => {
+  return (
+    <TransitionGroupGrid component="ul">
+      {feedItem.map((card, i) => {
+        const delay = i * 100
+
+        return (
+          <CSSTransition classNames="animation" key={card.id} timeout={1000 + delay}>
+            <AnimationList style={{ transitionDelay: `${delay}ms` }}>
+              <FeedCard
+                title={card.title}
+                user={card.user}
+                time={card.time_ago}
+                comments={card.comments_count}
+              />
+            </AnimationList>
+          </CSSTransition>
+        )
+      })}
+    </TransitionGroupGrid>
+  )
+}
 
 export const Container: React.FC = () => {
   const { feedItem } = useSelector((state: RootState) => state.news, shallowEqual)
@@ -32,11 +41,26 @@ export const Container: React.FC = () => {
   return <View feedItem={feedItem} />
 }
 
-const LayoutGrid = styled.ul`
+const TransitionGroupGrid = styled(TransitionGroup)`
   display: grid;
   ${media.desktop`
     grid-template-columns: repeat(auto-fit, 300px);
     justify-content: center;
     grid-gap: 20px;
   `}
+`
+
+const AnimationList = styled.li`
+  display: block;
+
+  &.animation-enter {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+
+  &.animation-enter-active {
+    opacity: 1;
+    transition: opacity 1s, transform 1s;
+    transform: translateY(0);
+  }
 `
